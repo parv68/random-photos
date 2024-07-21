7477f6b9d941fce2f701c8a9802bfef7008303d1
Parv <parvruhil68@gmail.com>
2024-07-21 10:12:17 +0530

    script done for js file

---
+++ script.js
@@ -0,0 +1,16 @@
+const imageContainerEl = document.querySelector('.image-container')
+const btnEl = document.querySelector('.btn')
+btnEl.addEventListener("click", () => {
+    imageNum = 10;
+    addNewImages();
+});
+
+function addNewImages(){
+    for (let i = 0; i < imageNum; i++) {
+       const imageEl = document.createElement('img');
+       imageEl.src = `https://picsum.photos/300?random=${Math.floor(
+        Math.random() * 1000
+       )}`;
+       imageContainerEl.appendChild(imageEl);
+    }   
+}
